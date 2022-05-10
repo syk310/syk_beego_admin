bdcd98440a8066fa021fcb31230b779d1c8dbeea
syk310 <abc1230594@qq.com>
2022-05-10 17:23:21 +0800

    testcontroller

--- controllers/test.go
+++ controllers/test.go
@@ -3,48 +3,14 @@ syk 练手项目
  */
 package controllers
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/syk310/syk_beego_admin/models"
-)
-
 type TestController struct {
 	BaseController
 }
 
 func (self *TestController) List() {
-	self.Data["pageTitle"] = "API文档"
-	self.Data["ts"] = time.Now()
-
-	grouplists := groupLists()
-	self.Data["grouplists"] = grouplists
-
-	groupId, _ := self.GetInt("id", 1)
-	groupInfo, err := models.GroupGetById(groupId)
-
-	if err != nil {
-		fmt.Println("数据不存在")
-	}
-
-	//公共文档
-	apiPublic, err := models.ApiPublicGetByIds(groupInfo.ApiPublicIds)
-	self.Data["apiPublic"] = apiPublic
-
-	//环境
-	// env, err := models.EnvGetByIds(groupInfo.EnvIds)
-	// self.Data["env"] = env
-
-	// //状态码
-	// code, err := models.CodeGetByIds(groupInfo.CodeIds)
-	// self.Data["code"] = code
-
-	//接口
-	apiMenu, _ := models.ApiTreeData(groupId)
-	self.Data["apiMenu"] = apiMenu
-	self.Data["groupId"] = groupId
-
-	self.TplName = "apidoc/index.html"
+	self.Data["pageTitle"] = "API接口"
+	self.Data["ApiCss"] = true
+	self.Data["auditStatus"] = AUDIT_STATUS_TEXT
+	self.display()
 }
 
